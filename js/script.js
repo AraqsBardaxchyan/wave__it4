e93b38b4bd88bf1d14febda94dbb4a3e962ed265
Araqsya Bardaxchyan <Araqsya.Bardaxchyan@mail.ru>
2020-05-16 00:11:53 +0300

    wave__it4

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const modal=document.querySelector('#modal_tel'),
+  bodyOverflow=document.querySelector('body');
+    document.querySelector('#open__modal').addEventListener('click',()=>{
+      modal.classList.remove('d-none');
+      modal.classList.add('d-flex');
+      bodyOverflow.style.overflow = 'hidden';
+    })
+    document.querySelector('#close').addEventListener('click',()=>{
+      modal.classList.remove('d-flex');
+      modal.classList.add('d-none');
+      bodyOverflow.removeAttribute('style');
+    });
+
+
+jQuery(function($){
+   $("#phone").mask("+7 (999) 999-9999");
+  
+});
